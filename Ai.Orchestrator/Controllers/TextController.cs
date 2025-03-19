6f1ff558bfd1ddee11eee72a3db3fa252b96c82b
smartguy05 <smartguy05@gmail.com>
2025-03-19 11:03:12 -0600

    Add conversation id to request body

--- Ai.Orchestrator/Controllers/TextController.cs
+++ Ai.Orchestrator/Controllers/TextController.cs
@@ -23,7 +23,8 @@ public class TextController : ControllerBase
         {
             textRequest.SystemPrompt,
             textRequest.UserPrompt,
-            Model = "gpt-4o"
+            Model = "gpt-4o", // todo: make this a setting
+            textRequest.ConversationId
         };
         var stringified = JsonSerializer.Serialize(serviceRequest);
         var request = new OrchestratorRequest
